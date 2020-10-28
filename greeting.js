91f5c16933456c8aef4eed8bd62ad5cddf0626d0
azurchief <67046863+azurchief@users.noreply.github.com>
2020-10-28 22:54:41 +0900

    checkpoint

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) { //3.3 , 3 //
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) { //3.3,  2 //
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() { // 3.3, 1 //
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -14,8 +30,9 @@ function paintGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-        // e.g. if not then -> else //
-    } else {
+        // Attention! -> askForName right below's role is: to call this function here, to let user to input in that input box(where placeholder displays)//
+        askForName();
+    } else { // otherwise, calling paintGreeting function. role is, to remove SHOWING_CN. //
         paintGreeting(currentUser);
     }
 }
