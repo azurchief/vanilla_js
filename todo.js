ab7ab7b55ee77d95e5416fda6393305759e503ab
azurchief <67046863+azurchief@users.noreply.github.com>
2020-10-30 00:36:35 +0900

    checkpoint

--- todo.js
+++ todo.js
@@ -1,18 +1,33 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList");
+  toDoList = document.querySelector(".js-toDoList"); //toDoList 는 js-toDoList 와 같음//
 
 const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos(){
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //[JSON.stringify] method will make an object/value to string. so, stringify!)
+}
 
 function paintToDo(text) {
   const li = document.createElement("li"); // li can be anything, e.g. potato whatever//
   const delBtn = document.createElement("button");
-  delBtn.innerText = "❌";
   const span = document.createElement("span");
+  const newID = toDos.length + 1;
+  delBtn.innerText = "❌";
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
-  toDoList.appendChild(li);
+  li.id = newId;
+  toDoList.appendChild(li); // li 를 ul에다가 append 하는 것! 해석: toDoList(ul, 위 3번줄 참고) 에다가 li를 append 함.//
+  //The append() method will insert DOMString (한번 언급 되었음! theory 파트에서.) objects as Text nodes.//
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDosObj);
+  saveToDos()
 }
 
 function handleSubmit(event) {
@@ -22,10 +37,20 @@ function handleSubmit(event) {
   toDoInput.value = "";
 }
 
+//function something(toDo) {
+ // console.log(toDo.text);
+// }
+
 function loadToDos() {
   const toDos = localStorage.getItem(TODOS_LS);
   if (toDos !== null) {
+      console.log(loadedToDos)
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDo){
+      paintToDo(toDo.text);
+    });
   }
+
 }
 
 function init() {
