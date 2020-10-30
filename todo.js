1ab69972feb1f4454374a94eaabbac979620abb6
azurchief <67046863+azurchief@users.noreply.github.com>
2020-10-30 23:49:26 +0900

    not bad!

--- todo.js
+++ todo.js
@@ -1,33 +1,46 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
-  toDoList = document.querySelector(".js-toDoList"); //toDoList 는 js-toDoList 와 같음//
+  toDoList = document.querySelector(".js-toDoList");
+  //toDoList 는 js-toDoList 와 같음//
 
-const TODOS_LS = "toDos";
+  const TODOS_LS = "toDos";
 
-const toDos = [];
+  let toDos = [];
 
-function saveToDos(){
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); //[JSON.stringify] method will make an object/value to string. so, stringify!)
-}
+  function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+      toDoList.removeChild(li);
+      const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+      });
+      toDos = cleanToDos;
+      saveToDos();
+  }
+  
+  function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+  }
 
 function paintToDo(text) {
-  const li = document.createElement("li"); // li can be anything, e.g. potato whatever//
+  const li = document.createElement("li"); /* li can be anything, e.g. potato whatever*/
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
-  const newID = toDos.length + 1;
+  const newId = toDos.length + 1; // capital letter error, camelcase //
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
   li.id = newId;
-  toDoList.appendChild(li); // li 를 ul에다가 append 하는 것! 해석: toDoList(ul, 위 3번줄 참고) 에다가 li를 append 함.//
-  //The append() method will insert DOMString (한번 언급 되었음! theory 파트에서.) objects as Text nodes.//
+  toDoList.appendChild(li); /* li 를 ul에다가 append 하는 것! 해석: toDoList(ul, 위 3번줄 참고) 에다가 li를 append 함.*/
+  /*The append() method will insert DOMString (한번 언급 되었음! theory 파트에서.) objects as Text nodes.*/
   const toDoObj = {
     text: text,
     id: newId
   };
-  toDos.push(toDosObj);
-  saveToDos()
+  toDos.push(toDoObj); // spelling mistake //
+  saveToDos(); /* my mistake 1 : forgot to insert [;]! */
 }
 
 function handleSubmit(event) {
@@ -37,15 +50,15 @@ function handleSubmit(event) {
   toDoInput.value = "";
 }
 
-//function something(toDo) {
- // console.log(toDo.text);
-// }
+//function something(toDo) {//
+ // console.log(toDo.text);//
+// } //
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (loadedtoDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) { /* mistake 2 : forgot carmelcase */
     const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo){
+    parsedToDos.forEach(function(toDo) {
       paintToDo(toDo.text);
     });
   }
